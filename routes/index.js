2a46a7ade027790883bc83535c4fe82117817047
chen-xt <1250548605@qq.com>
2016-10-29 15:59:32 +0800

    login3

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose'); // 导入组件
+var models = require('./models'); 	// 导入自定义组件
+var User = models.User; //使用User模型，对应的users表
+mongoose.connect('mongodb://localhost/mldndb'); //连接数据库
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,14 +15,26 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: '用户登录' });
 });
 
-router.post('/doLogin', function(req, res, next) {
+/*router.post('/doLogin', function(req, res, next) {
   var user = {userid:'chen' , password:'123'} ;	// 固定数据
 	if (req.body.userid==user.userid && req.body.password==user.password) {
 		res.redirect("/welcome?uid=" + req.body.userid) ;	// 地址重写
 	}else{
 	res.redirect("/login") ;
 	}
+});*/
+router.post('/doLogin', function(req, res, next) {
+    var query_doc = {userid:req.body.userid , password:req.body.password} ;	// 固定数据
+	User.count(query_doc,function(err,doc){// 直接利用mongodb的命令进行操作
+		if(doc == 0) {	// 输入错误，没有此信息
+			res.redirect("/login") ;
+		} 
+		else {	// 成功
+			res.redirect("/welcome?uid=" + req.body.userid) ;	// 地址重写
+		}
+	});
 });
+
 
 router.get('/logout', function(req, res, next) {
   res.render('login', { title: '用户注销' });
